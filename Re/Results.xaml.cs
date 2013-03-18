34db2bf948c47eb398926e413627e7b02be6b4dd
Everett Carney <everett@mentaltangent.net>
2013-03-17 17:27:42 -0700

    MultiSelectorList and Websites work

--- Re/Results.xaml.cs
+++ Re/Results.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -8,6 +9,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -24,17 +26,24 @@ namespace Re
 
     public partial class Results : PhoneApplicationPage
     {
-
+        // Privates
         ResultViewModel _viewModel;
         static List<Keyword> queryList = new List<Keyword>();
         const int BUFFER_SIZE = 1024;
         private string _searchTerm = "";
+
+        // ApplicationBar buttons
+        ApplicationBarIconButton resultMultiSelectButton;
+        ApplicationBarIconButton reSearchButton;
+        ApplicationBarIconButton openMultiPageButton;
 
         public Results()
         {
             InitializeComponent();
             _viewModel = (ResultViewModel)Resources["viewModel"];
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+            CreateSelectionResultsApplicationBar();
+            OnResultsActivated();
         }
 
         void MainPage_Loaded(object sender, RoutedEventArgs e)
@@ -87,6 +96,129 @@ namespace Re
 
             _viewModel.FetchSearchResults(_searchTerm);
             // gvwQuery.ItemsSource = queryList.Take(7); // This is the databinding for the query text
+        }
+
+        private void lbxOutput_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateApplicationBar();
+        }
+
+        private void OnGridSelectorIsSelectionEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            SetupSelectionApplicationBar();
+        }
+
+        // Setup the application bar
+        #region AppBar
+        private void CreateSelectionResultsApplicationBar()
+        {
+            reSearchButton = new ApplicationBarIconButton();
+            reSearchButton.IconUri = new Uri("/Toolkit.Content/ApplicationBar.Check.png", UriKind.RelativeOrAbsolute);
+            reSearchButton.Text = "Re:search";
+            reSearchButton.Click += reSearchButton_Click;
+
+            resultMultiSelectButton = new ApplicationBarIconButton();
+            resultMultiSelectButton.IconUri = new Uri("/Toolkit.Content/ApplicationBar.Select.png", UriKind.RelativeOrAbsolute);
+            resultMultiSelectButton.Text = "select";
+            resultMultiSelectButton.Click += resultMultiSelectButton_Click;
+
+            openMultiPageButton = new ApplicationBarIconButton();
+            openMultiPageButton.IconUri = new Uri("/Toolkit.Content/ApplicationBar.Upload.png", UriKind.RelativeOrAbsolute);
+            openMultiPageButton.Text = "open";
+            openMultiPageButton.Click += openMultiPageButton_Click;
+
+        }
+
+        private void OnResultsActivated()
+        {
+            if (lbxOutput.IsSelectionEnabled)
+            {
+                lbxOutput.IsSelectionEnabled = false; // Will also update the Application Bar
+            }
+            else
+            {
+                SetupSelectionApplicationBar();
+            }
+        }
+
+        private void SetupSelectionApplicationBar()
+        {
+            ClearApplicationBar();
+            if (lbxOutput.IsSelectionEnabled)
+            {
+                ApplicationBar.Buttons.Add(reSearchButton);
+                //ApplicationBar.Buttons.Add(openMultiPageButton);
+                UpdateApplicationBar();
+            }
+            else
+            {
+                ApplicationBar.Buttons.Add(resultMultiSelectButton);
+            }
+            ApplicationBar.IsVisible = true;
+        }
+
+        void UpdateApplicationBar()
+        {
+            reSearchButton.IsEnabled = ((lbxOutput.SelectedItems != null) && (lbxOutput.SelectedItems.Count > 0));
+            //openMultiPageButton.IsEnabled = ((lbxOutput.SelectedItems != null) && (lbxOutput.SelectedItems.Count > 0));
+        }
+
+        void openMultiPageButton_Click(object sender, EventArgs e)
+        {
+            LaunchWebSelection();
+        }
+
+        private async Task LaunchWebSelection()
+        {
+            bool success = false;
+            foreach (WebResult result in lbxOutput.SelectedItems)
+            {
+                success = await Windows.System.Launcher.LaunchUriAsync(new Uri(@result.Url));
+                if (success)
+                    Debug.WriteLine("Success");
+                else
+                    Debug.WriteLine("Failure");
+            }
+        }
+
+        void reSearchButton_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Pressed the reSearchButton");
+        }
+
+        void resultMultiSelectButton_Click(object sender, EventArgs e)
+        {
+            lbxOutput.EnforceIsSelectionEnabled = true;
+        }
+
+        void ClearApplicationBar()
+        {
+            while (ApplicationBar.Buttons.Count > 0)
+            {
+                ApplicationBar.Buttons.RemoveAt(0);
+            }
+
+            while (ApplicationBar.MenuItems.Count > 0)
+            {
+                ApplicationBar.MenuItems.RemoveAt(0);
+            }
+        }
+
+        protected override void OnBackKeyPress(CancelEventArgs e)
+        {
+            if (lbxOutput.IsSelectionEnabled)
+            {
+                lbxOutput.EnforceIsSelectionEnabled = false;
+                e.Cancel = true;
+            }
+        }
+        #endregion
+
+        private void ResultItemSelected(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            StackPanel _r = (StackPanel)sender;
+            WebResult _result = (WebResult)_r.DataContext;
+            Windows.System.Launcher.LaunchUriAsync(new Uri(@_result.Url));
         }
     }
 }
